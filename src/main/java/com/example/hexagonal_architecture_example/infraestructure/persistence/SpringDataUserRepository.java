72dfb089c15dbcb1e1af7b4e721ad377bccf45d5
BralDev <jesusbrayanjoaquin@gmail.com>
2026-01-26 23:39:56 -0500

    feat: remove unused pagination methods from SpringDataUserRepository

--- src/main/java/com/example/hexagonal_architecture_example/infraestructure/persistence/SpringDataUserRepository.java
+++ src/main/java/com/example/hexagonal_architecture_example/infraestructure/persistence/SpringDataUserRepository.java
@@ -1,11 +1,8 @@
 package com.example.hexagonal_architecture_example.infraestructure.persistence;
 
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface SpringDataUserRepository extends JpaRepository<UserEntity, Long>, JpaSpecificationExecutor<UserEntity> {
-    Page<UserEntity> findByFirstNameContainingIgnoreCase(String firstName, Pageable pageable);
-    Page<UserEntity> findByLastNameContainingIgnoreCase(String lastName, Pageable pageable);
+
 }
